6e36911a422a8ab092d4ac3e8c6d38cd86ac635d
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-15 23:33:24 +0300

    add implementation of JwtTokenGenerator

---
+++ Task Manager.Identity.Infrastructure/Options/JwtOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Task_Manager.Identity.Infrastructure.Options;
+
+public sealed record JwtOptions(
+    string SecretKey,
+    string Issuer,
+    string Audience
+);
---
+++ Task Manager.Identity.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Task_Manager.Identity.Application.Jwt;
+using Task_Manager.Identity.Core.Entities;
+using Task_Manager.Identity.Infrastructure.Options;
+
+namespace Task_Manager.Identity.Infrastructure.Services;
+
+public class JwtTokenGenerator(
+    JwtOptions options
+) : IJwtTokenGenerator
+{
+    private readonly JwtOptions _options = options;
+
+    public JwtToken GenerateToken(ApplicationUser user, DateTimeOffset issuedAt, TimeSpan expiresIn)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Name, user.DisplayName),
+            new Claim(JwtRegisteredClaimNames.Iat, issuedAt.ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: _options.Issuer,
+            audience: _options.Audience,
+            claims: claims,
+            notBefore: issuedAt.UtcDateTime,
+            expires: issuedAt.Add(expiresIn).UtcDateTime,
+            signingCredentials: creds
+        );
+
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return new JwtToken(
+            accessToken,
+            "", // TODO: Implement refresh token generation
+            issuedAt,
+            expiresIn
+        );
+    }
+}
