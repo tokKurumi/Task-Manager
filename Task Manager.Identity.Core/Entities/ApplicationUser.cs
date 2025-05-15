6ad2922a2f3577cb7bed92675a31e5d2b9708d15
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-15 22:17:23 +0300

    add abstraction layer to connect infra with core

--- Task Manager.Identity.Core/Entities/ApplicationUser.cs
+++ Task Manager.Identity.Core/Entities/ApplicationUser.cs
@@ -2,6 +2,15 @@
 
 namespace Task_Manager.Identity.Core.Entities;
 
+public interface IUserData
+{
+    Guid Id { get; }
+    string Email { get; }
+    string DisplayName { get; }
+    string PasswordHash { get; }
+    DateTimeOffset CreatedAt { get; }
+}
+
 public class ApplicationUser
 {
     public Guid Id { get; init; }
@@ -13,6 +22,15 @@ public class ApplicationUser
     private ApplicationUser(string email, string displayName, string passwordHash, DateTimeOffset createdAt)
     {
         Id = Guid.CreateVersion7();
+        Email = email;
+        DisplayName = displayName;
+        PasswordHash = passwordHash;
+        CreatedAt = createdAt;
+    }
+
+    public ApplicationUser(Guid id, string email, string displayName, string passwordHash, DateTimeOffset createdAt)
+    {
+        Id = id;
         Email = email;
         DisplayName = displayName;
         PasswordHash = passwordHash;
@@ -36,7 +54,31 @@ public class ApplicationUser
             return Result<ApplicationUser, ApplicationUserError>.Failure(new EmptyPasswordHashError());
         }
 
-        return Result<ApplicationUser, ApplicationUserError>.Success(new ApplicationUser(email, displayName, passwordHash, timeProvider.GetUtcNow()));
+        return Result<ApplicationUser, ApplicationUserError>.Success(
+            new ApplicationUser(email, displayName, passwordHash, timeProvider.GetUtcNow())
+        );
+    }
+
+    public static Result<ApplicationUser, ApplicationUserError> TryCreate(IUserData userData, TimeProvider timeProvider)
+    {
+        if (string.IsNullOrWhiteSpace(userData.Email))
+        {
+            return Result<ApplicationUser, ApplicationUserError>.Failure(new EmptyEmailError());
+        }
+
+        if (string.IsNullOrWhiteSpace(userData.DisplayName))
+        {
+            return Result<ApplicationUser, ApplicationUserError>.Failure(new EmptyDisplayNameError());
+        }
+
+        if (string.IsNullOrEmpty(userData.PasswordHash))
+        {
+            return Result<ApplicationUser, ApplicationUserError>.Failure(new EmptyPasswordHashError());
+        }
+
+        return Result<ApplicationUser, ApplicationUserError>.Success(
+            new ApplicationUser(userData.Id, userData.Email, userData.DisplayName, userData.PasswordHash, userData.CreatedAt)
+        );
     }
 }
 
