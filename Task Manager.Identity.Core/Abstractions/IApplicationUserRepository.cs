a5123d9463f16ba1590ef0107da9c5659342500f
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-15 19:14:27 +0300

    create abstractions for app user repository and password service

---
+++ Task Manager.Identity.Core/Abstractions/IApplicationUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Task_Manager.Common;
+using Task_Manager.Identity.Core.Entities;
+
+namespace Task_Manager.Identity.Core.Abstractions;
+
+public interface IApplicationUserRepository
+{
+    Task<Result<ApplicationUser, ApplicationUserRepositoryError>> CreateUserAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default);
+    Task<ApplicationUser?> FindByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<ApplicationUser?> FindByIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<Page<ApplicationUser>> GetPageAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+}
+
+public abstract record ApplicationUserRepositoryError(string Code) : Error(Code);
---
+++ Task Manager.Identity.Core/Abstractions/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Task_Manager.Identity.Core.Abstractions;
+
+public interface IPasswordService
+{
+    string HashPassword(string password);
+    bool VerifyHashedPassword(string hashedPassword, string providedPassword);
+}
