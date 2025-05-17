1e29cfa17371076f6b652976924aa69284557156
tokKurumi <tkurumi901@gmail.com>
2025-05-17 14:05:41 +0300

    change npgsql to aspire support for infra

--- Task Manager.Identity.Infrastructure/Extensions/IdentityServiceExtensions.cs
+++ Task Manager.Identity.Infrastructure/Extensions/IdentityServiceExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Task_Manager.Identity.Core.Abstractions;
 using Task_Manager.Identity.Core.Entities;
 using Task_Manager.Identity.Infrastructure.Data;
@@ -12,20 +12,11 @@ namespace Task_Manager.Identity.Infrastructure.Extensions;
 
 public static class IdentityServiceExtensions
 {
-    public static IServiceCollection AddIdentityDbContext(this IServiceCollection services, IConfiguration configuration)
+    public static IHostApplicationBuilder AddIdentityDbContext(this IHostApplicationBuilder builder, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("IdentityDbContext")
-            ?? throw new InvalidOperationException("Connection string 'IdentityDbContext' not found.");
+        builder.AddNpgsqlDbContext<ApplicationIdentityDbContext>("identity-postgres"); // TODO: remove this hard-code
 
-        services.AddDbContext<ApplicationIdentityDbContext>(options =>
-        {
-            options.UseNpgsql(connectionString, npgsqlOptions =>
-            {
-                npgsqlOptions.MigrationsAssembly(typeof(ApplicationIdentityDbContext).Assembly.FullName);
-            });
-        });
-
-        services.AddIdentityCore<ApplicationUser>(options =>
+        builder.Services.AddIdentityCore<ApplicationUser>(options =>
         {
             options.Password.RequireDigit = true;
             options.Password.RequiredLength = 8;
@@ -35,10 +26,10 @@ public static class IdentityServiceExtensions
         .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
         .AddTokenProvider<DataProtectorTokenProvider<UserEntity>>("Default");
 
-        services.AddScoped<RoleManager<IdentityRole<Guid>>>();
+        builder.Services.AddScoped<RoleManager<IdentityRole<Guid>>>();
 
-        services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
+        builder.Services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
 
-        return services;
+        return builder;
     }
 }
