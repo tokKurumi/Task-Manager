1fb28a33e3336c60ac572e6515bba03869a3e634
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-19 03:01:25 +0300

    configure prefix & add versioning

---
+++ Task Manager.Identity.IdentityAPI/Extensions/MvcOptionsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Task_Manager.Identity.IdentityAPI.Extensions;
+
+public static class MvcOptionsExtensions
+{
+    public static void UseRoutePrefix(this MvcOptions opts, IRouteTemplateProvider routeAttribute)
+    {
+        opts.Conventions.Add(new RoutePrefixConvention(routeAttribute));
+    }
+
+    public static void UseRoutePrefix(this MvcOptions opts, [StringSyntax("Route")] string prefix)
+    {
+        opts.UseRoutePrefix(new RouteAttribute(prefix));
+    }
+}
+
+public class RoutePrefixConvention(
+    IRouteTemplateProvider route
+) : IApplicationModelConvention
+{
+    private readonly AttributeRouteModel _routePrefix = new(route);
+
+    public void Apply(ApplicationModel application)
+    {
+        foreach (var selector in application.Controllers.SelectMany(controller => controller.Selectors))
+        {
+            if (selector.AttributeRouteModel is not null)
+            {
+                selector.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(_routePrefix, selector.AttributeRouteModel);
+                continue;
+            }
+
+            selector.AttributeRouteModel = _routePrefix;
+        }
+    }
+}
--- Task Manager.Identity.IdentityAPI/Program.cs
+++ Task Manager.Identity.IdentityAPI/Program.cs
@@ -1,5 +1,7 @@
+using Asp.Versioning;
 using Scalar.AspNetCore;
 using Task_Manager.Identity.Application;
+using Task_Manager.Identity.IdentityAPI.Extensions;
 using Task_Manager.Identity.Infrastructure;
 using Task_Manager.ServiceDefaults;
 
@@ -7,21 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.UseRoutePrefix("api/v{version:apiVersion}");
+});
 builder.Services.AddOpenApi();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+})
+.AddMvc()
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
 
 builder.AddApplication();
 builder.AddInfrastructure();
 
 var app = builder.Build();
 
+app.MapDefaultEndpoints();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-
-app.MapDefaultEndpoints();
 
 app.UseHttpsRedirection();
 
