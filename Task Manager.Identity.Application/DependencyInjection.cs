cbb873e0d0d8c8c7d2033b928306d6084575af8c
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-20 02:48:29 +0300

    Remove TracingBehavior from DependencyInjection

--- Task Manager.Identity.Application/DependencyInjection.cs
+++ Task Manager.Identity.Application/DependencyInjection.cs
@@ -2,7 +2,6 @@
 using Mediator;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Diagnostics;
 using Task_Manager.Common;
 using Task_Manager.Identity.Application.Services;
 using Task_Manager.Identity.Application.Services.Abstractions;
@@ -42,7 +41,6 @@ public static class DependencyInjection
             config.ServiceLifetime = ServiceLifetime.Scoped;
         });
 
-        builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(TracingBehavior<,>));
         builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
         builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(CommandValidationBehavior<,>));
 
@@ -55,21 +53,6 @@ public static class DependencyInjection
         });
 
         return builder;
-    }
-}
-
-public class TracingBehavior<TMessage, TResponse>(
-    ActivitySource activitySource
-) : IPipelineBehavior<TMessage, TResponse>
-    where TMessage : notnull, IMessage
-{
-    private readonly ActivitySource _activitySource = activitySource;
-
-    public ValueTask<TResponse> Handle(TMessage message, CancellationToken cancellationToken, MessageHandlerDelegate<TMessage, TResponse> next)
-    {
-        using var activity = _activitySource.StartActivity($"Handling {typeof(TMessage).Name}");
-
-        return next(message, cancellationToken);
     }
 }
 
