dc64fe5f93d3464f55f04b9729f5ee6aa4463e40
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-19 16:11:25 +0300

    add tracing pipeline for Mediator

--- Task Manager.Identity.Application/DependencyInjection.cs
+++ Task Manager.Identity.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Mediator;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 using Task_Manager.Identity.Application.Services;
 using Task_Manager.Identity.Application.Services.Abstractions;
 
@@ -28,6 +30,23 @@ public static class DependencyInjection
             config.ServiceLifetime = ServiceLifetime.Scoped;
         });
 
+        builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(TracingBehavior<,>));
+
         return builder;
+    }
+}
+
+public class TracingBehavior<TMessage, TResponse>(
+    ActivitySource activitySource
+) : IPipelineBehavior<TMessage, TResponse>
+    where TMessage : notnull, IMessage
+{
+    private readonly ActivitySource _activitySource = activitySource;
+
+    public ValueTask<TResponse> Handle(TMessage message, CancellationToken cancellationToken, MessageHandlerDelegate<TMessage, TResponse> next)
+    {
+        using var activity = _activitySource.StartActivity($"Handling {typeof(TMessage).Name}");
+
+        return next(message, cancellationToken);
     }
 }
