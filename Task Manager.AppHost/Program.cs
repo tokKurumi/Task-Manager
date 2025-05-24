372328f7e894d253cf709c8ba9382be83983a03c
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-24 06:36:06 +0300

    add RabbitMQ resource

--- Task Manager.AppHost/Program.cs
+++ Task Manager.AppHost/Program.cs
@@ -5,6 +5,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var jwtParameters = builder.AddJwtParameters();
 
+var rabbitMq = builder
+    .AddRabbitMQ(MessageBroker.RabbitMQ)
+    .WithDataVolume(MessageBroker.RabbitMQVolume)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithManagementPlugin();
+
 #region identity
 var identityDatabase = builder
     .AddPostgres(IdentityProject.PostgreSQLResource)
@@ -17,13 +23,16 @@ var identityMigrator = builder
     .AddProject<Projects.Task_Manager_Identity_Migrator>(IdentityProject.Migrator)
     .WithJwtParameters(jwtParameters)
     .WithReference(identityDatabase)
+    .WithReference(rabbitMq)
     .WaitFor(identityDatabase);
 
 var identity = builder
     .AddProject<Projects.Task_Manager_Identity_IdentityAPI>(IdentityProject.API)
     .WithJwtParameters(jwtParameters)
     .WithReference(identityDatabase)
+    .WithReference(rabbitMq)
     .WaitFor(identityDatabase)
+    .WaitFor(rabbitMq)
     .WaitForCompletion(identityMigrator);
 #endregion
 
