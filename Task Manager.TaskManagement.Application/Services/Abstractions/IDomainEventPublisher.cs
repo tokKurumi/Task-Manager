44190f4f45709330265252742f9e5e606e3ceae2
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-28 06:07:11 +0300

    implement unit of work for TaskManagment

--- Task Manager.TaskManagement.Application/Services/Abstractions/IDomainEventPublisher.cs
+++ Task Manager.TaskManagement.Application/Services/Abstractions/IDomainEventPublisher.cs
@@ -1,9 +1,9 @@
-﻿using Task_Manager.Identity.Core.Entities;
+﻿//using Task_Manager.Identity.Core.Entities;
 
-namespace Task_Manager.TaskManagement.Application.Services.Abstractions;
+//namespace Task_Manager.TaskManagement.Application.Services.Abstractions;
 
-public interface IDomainEventPublisher
-{
-    Task PublishAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default);
-    Task PublishManyAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
-}
+//public interface IDomainEventPublisher
+//{
+//    Task PublishAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default);
+//    Task PublishManyAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+//}
