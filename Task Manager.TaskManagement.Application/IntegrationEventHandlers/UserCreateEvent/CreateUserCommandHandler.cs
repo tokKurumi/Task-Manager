08d50dfbc8ae8146bdba8eb75f49413a92207c15
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-28 07:24:09 +0300

    use unit of work

--- Task Manager.TaskManagement.Application/IntegrationEventHandlers/UserCreateEvent/CreateUserCommandHandler.cs
+++ Task Manager.TaskManagement.Application/IntegrationEventHandlers/UserCreateEvent/CreateUserCommandHandler.cs
@@ -1,18 +1,24 @@
 ﻿using Mediator;
 using Task_Manager.Common;
 using Task_Manager.TaskManagement.Application.Services;
+using Task_Manager.TaskManagement.Application.Services.Abstractions;
 
 namespace Task_Manager.TaskManagement.Application.IntegrationEventHandlers.UserCreateEvent;
 
 public sealed class CreateUserCommandHandler(
+    IUnitOfWork unitOfWork,
     IUserApplicationService userApplicationService
 ) : ICommandHandler<CreateUserCommand, Result<CreateUserResponse, UserApplicationError>>
 {
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly IUserApplicationService _userApplicationService = userApplicationService;
 
     public async ValueTask<Result<CreateUserResponse, UserApplicationError>> Handle(CreateUserCommand command, CancellationToken cancellationToken)
     {
-        return await _userApplicationService.CreateUserAsync(UserCreateCommandMapper.ToUserApplicationServiceModel(command), cancellationToken)
-            .Map(UserCreateCommandMapper.ToUseCaseModel);
+        return await _unitOfWork.ExecuteWithStrategyAsync(async ct =>
+        {
+            return await _userApplicationService.CreateUserAsync(UserCreateCommandMapper.ToUserApplicationServiceModel(command), ct)
+                .Map(UserCreateCommandMapper.ToUseCaseModel);
+        }, cancellationToken);
     }
 }
