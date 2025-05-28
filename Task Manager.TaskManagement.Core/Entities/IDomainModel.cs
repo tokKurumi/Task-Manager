5d1bb8bb0b5b0690b5c6c03cd0d1d8aa6c9938fd
Oleg Yudashkin <tkurumi901@gmail.com>
2025-05-28 04:45:32 +0300

    prettify domain model

--- Task Manager.TaskManagement.Core/Entities/IDomainModel.cs
+++ Task Manager.TaskManagement.Core/Entities/IDomainModel.cs
@@ -1,7 +1,12 @@
 ﻿namespace Task_Manager.TaskManagement.Core.Entities;
 
-public interface IDomainModel<in TData, out TDomainEntity>
+public interface IDomainModel
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+}
+
+public interface IDomainModel<in TData, out TDomainEntity> : IDomainModel
+    where TDomainEntity : IDomainModel
 {
     static abstract TDomainEntity ConvertFromData(TData input);
-    IReadOnlyList<IDomainEvent> DomainEvents { get; }
 }
